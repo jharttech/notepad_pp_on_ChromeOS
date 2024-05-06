98787adeebcba16865964750dafe8a6eb6bb1264
jharttech <jharttech@hotmail.com>
2024-05-06 14:49:35 -0500

    Fine tuning automation tasks.

--- install.sh
+++ install.sh
@@ -19,7 +19,8 @@ sudo snap install core notepad-plus-plus
 #Verify notepad snap was installed
 if [[ -f /var/lib/snapd/desktop/applications/notepad-plus-plus_notepad-plus-plus.desktop ]]; then 
 	#Create a desktop icon
-	sudo cp /var/lib/snapd/desktop/applications/notepad-plus-plus_notepad-plus-plus.desktop /usr/share/applications/
+	sudo cp /var/lib/snapd/desktop/applications/notepad-plus-plus_notepad-plus-plus.desktop /var/lib/snapd/desktop/applications/notepad-plus-plus_notepad-plus-plus.desktop.old
+	sudo ln -s /var/lib/snapd/desktop/applications/notepad-plus-plus_notepad-plus-plus.desktop /usr/share/applications/
 else
 	echo "Notepad++ was not correctly installed.  Exiting script now!!"
 	exit 1
@@ -30,21 +31,24 @@ fi
 #Open a terminal session each login.
 #We can only use sudo this way as the linux vm in chromeOS has no root password.
 cd /opt
-sudo touch launch_notepad-plus-plus.sh
-sudo chmod 777 launch_notepad-plus-plus.sh
-sudo echo "#!/bin/bash
+sudo tee launch_notepad-plus-plus.sh << EOF
+#!/bin/bash
+
 #set xhost access
 xhost +
+
 #launch notepad-plus-plus snap
-Exec=env BAMF_DESKTOP_FILE_HINT=/var/lib/snapd/desktop/applications/notepad-plus-plus_notepad-plus-plus.desktop /snap/bin/notepad-plus-plus %F" > launch_notepad-plus-plus.sh
-sudo chmod 755 launch_notepad-plus-plus.sh
+Exec=env BAMF_DESKTOP_FILE_HINT=/var/lib/snapd/desktop/applications/notepad-plus-plus_notepad-plus-plus.desktop /snap/bin/notepad-plus-plus %F
+EOF
+
 sudo chmod +x launch_notepad-plus-plus.sh
+
 cd ~/
 
 #Verify the desktop shortcut was created
 if [[ -f /usr/share/applications/notepad-plus-plus_notepad-plus-plus.desktop ]]; then
 	# replace the Exec line to point to needed script that will set the xhost access everytime the notepad++ snap is ran from the chromeOS launcher
-	sudo sed -i '/Exec/ s/\=env\ BAMF_DESKTOP_FILE_HINT\=\/var\/lib\/snapd\/desktop\/applications\/notepad-plus-plus_notepad-plus-plus\.desktop\ \/snap\/bin\/notepad-plus-plus\ \%F/\=\/opt\/launch_notepad-plus-plus.sh/' /usr/share/applications/notepad-plus-plus_notepad-plus-plus.desktop
+	sudo sed -i /Exec/ s#=env BAMF_DESKTOP_FILE_HINT=/var/lib/snapd/desktop/applications/notepad-plus-plus_notepad-plus-plus\.desktop /snap/bin/notepad-plus-plus %F#=/opt/launch_notepad-plus-plus.sh# /usr/share/applications/notepad-plus-plus_notepad-plus-plus.desktop
 	_line=$(cat /usr/share/applications/notepad-plus-plus_notepad-plus-plus.desktop | grep Exec)
 	if [[ $_line == "Exec=/opt/launch_notepad-plus-plus.sh" ]]; then
 		echo "All complete.  Use app drawer to find and run notepad++"
